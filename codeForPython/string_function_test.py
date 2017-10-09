f1f08b33970125db52bf0f72424632423f1d9138
DimonOuYang <825824032@qq.com>
2017-10-09 12:21:04 +0800

    test python function

--- codeForPython/string_function_test.py
+++ codeForPython/string_function_test.py
@@ -1,13 +1,26 @@
+'''
 str="hello python1"
-print(str.isalnum())
-print(str.isalpha())
-print(str.join(str,'-'))
-#print(str)
+print("-".join(str))
+print(str.zfill(40))
 
+print("hello")
+'''
+'''
+print(type(print))
+print(id(print))
 
+def add(num1,num2,num3):
+    return num1+num2+num3
+print(type(add))
+print(id(add))
 
-
-
+'''
+'''
+import os
+os.system("notepad")
+print(type(os.system))
+print(id(os.system))
+'''
 
 
 
